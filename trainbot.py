12f6d28ec1c6cf9da7d430ceb770aa10dea4ae14
Suryagayatri <97801981+sunstormer@users.noreply.github.com>
2022-01-29 11:28:33 +0530

    Add files via upload

---
+++ trainbot.py
@@ -0,0 +1,13 @@
+import random
+import json
+import pickle
+import numpy as n
+import nltk
+
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.models import load_model
+
